039951ca920ce1a44884719b4ff81c7d1dee1372
Ville Valkonen <weezelding@gmail.com>
2018-03-05 20:41:58 +0200

    And now with working chooser and with fixed template

--- app.py
+++ app.py
@@ -10,25 +10,36 @@ db = SQLAlchemy(app)
 
 class Present(db.Model):
     pid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Integer, unique=False, nullable=False)
+    name = db.Column(db.String(16), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
 
     def __repr__(self):
-        return "<Present %r>".format(self.present)
+        return "%r" % (self.name)
 
 @app.route("/", methods=["GET"])
 def root_view():
-    return render_template("index.html")
+    return render_template("index.html", presents=Present.query.all())
 
 @app.route("/", methods=["POST"])
 def root_post():
     params = request.form
     if "present" not in params or params["present"] == "":
         return "ERROR: Missing param\n"
-    present = Present(name=params["present"])
-    db.session.add(present)
-    db.session.commit()
     return ("Chose {}\n".format(params["present"]))
 
-if __name__ == '__main__':
+def dbinit():
     db.create_all()
-    app.run(debug=True, host="127.0.0.1")
+    available_presents = [ Present(name="kassi", description="Hieno kassi"),
+                           Present(name="lusikka", description="Puurolusikka"),
+                           Present(name="sushi", description="Nakkisushi"),
+                           Present(name="kieli", description="Jaappanian alkeet"),
+                           Present(name="tutti", description="Huvitutti"),
+                           Present(name="matka", description="New Yorkin matka"),
+                           Present(name="poni", description="Poni")
+                         ]
+    db.session.add_all(available_presents)
+    db.session.commit()
+
+if __name__ == '__main__':
+    dbinit()
+    app.run(debug=True, use_reloader=False, host="127.0.0.1")
